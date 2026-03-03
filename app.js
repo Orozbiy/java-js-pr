6267928903b5123a0191f82f14d52dee688e0d32
unknown <orozbijhodzebekov@gmail.com>
2026-03-03 18:55:41 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+	// готовые функсии
+	
+	// console.log("hello-wd");
+	// prompt('Write your name ')
+	// переменный
+	// 3 вида переменных var let const
+
+	// var bag = 'laptop внутри bag '
+	// var number = 25000
+	// let car = 'windows'
+	// const apple = 
+	
+	
+
+	// console.log(bag)
+	// console.log(number)
+	// console.log(car)
+	// console.log(apple)
+
+
+	// let fname = 'Khodzhebekov '
+
+	// let name1 = 'Orozbii '   
+	
+	// let age = '21'
+
+	// console.log(fname + name1 + age )
+
+
+// 	const blac =prompt('салам алейкум кандайсын?')
+//  const name0 = ''
+
+//   console.log(blac+name3)
+
+// 	const blak =prompt('Атын ким ?')
+// 	const black = ''
+
+// 	console.log(blak+black)
+// 	const dode =prompt('Кай жерден  болосун?')
+//    const frt = ''
+// 	 console.log(dode+frt)
+// 	const bobu =prompt('Иштейсинби же окуйсуну?') 
+
+// const frtr = ''
+//  console.log(bobu+frtr)
+// 	const back =prompt('Шаардын кайсы районуна жашайсын?')
+// const frtti = ''
+//  console.log(back+frtti)
+// 	const bag =prompt('Эмнеге кызыгасын ?')
+// const frtlk = ''
+//  console.log(bag+frtlk)
+
+
+const name1 = prompt('Сенин атын ким ?')
+
+const bbc = ''
+console.log( name1+bbc)
+const name2 = prompt(' Жактырган тамагын?')
+const bbbc = ''
+
+const name3 = prompt('Кайсы мамлекетке баргын келет  ?')
+const bbcl = ''
+
+const name4 = prompt('Эмне менен алексин?')
+const bbct = ''
+
+alert ("my name is "+name1+ bbc);
+
+alert ("Жактырган тамагын? "+ name2+ bbbc );
+
+alert ("Кайсы мамлекетке баргын келет "+name3+ bbcl);
+
+alert ("Эмне менен алексин? "+name4+ bbct);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
